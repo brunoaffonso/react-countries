ae271397e003c2027908a8c52366f96f330922dd
Bruno Affonso Teixeira <brunoaffonso@gmail.com>
2020-11-19 20:04:59 -0300

    build

---
+++ build/precache-manifest.6eba5eb57898ba1f9a4901172b888ac5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2446642496265a2078e9fa5083ec69fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc8791b5396b5e86b921",
+    "url": "/static/css/2.fec5649f.chunk.css"
+  },
+  {
+    "revision": "75afd385f93c2e7b2306",
+    "url": "/static/css/main.13bd69cd.chunk.css"
+  },
+  {
+    "revision": "cc8791b5396b5e86b921",
+    "url": "/static/js/2.4aa198e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4aa198e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75afd385f93c2e7b2306",
+    "url": "/static/js/main.c5c101aa.chunk.js"
+  },
+  {
+    "revision": "67e85333bb14618f3a58",
+    "url": "/static/js/runtime-main.d0c8e979.js"
+  }
+]);
